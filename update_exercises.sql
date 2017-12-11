9a0274751b6e7ab2fcdacf9777fa08e809ad55e8
MicCabeza22 <cabeza.michael@yahoo.com>
2017-12-11 14:19:21 -0600

    Formatted code to remove extra space.

--- update_exercises.sql
+++ update_exercises.sql
@@ -9,14 +9,9 @@ SELECT * FROM albums WHERE release_date < 1980;
 SELECT 'All albums by Michael Jackson' AS 'Caption';
 SELECT * FROM albums WHERE artist = 'Michael Jackson';
 
-UPDATE albums
-SET sales = sales * 10;
-
-UPDATE albums
-SET release_date = release_date - 100 WHERE release_date < 1980;
-
-UPDATE albums
-SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
+UPDATE albums SET sales = sales * 10;
+UPDATE albums SET release_date = release_date - 100 WHERE release_date < 1980;
+UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
 
 SELECT * FROM albums;
 SELECT * FROM albums WHERE release_date < 1980;

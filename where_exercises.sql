30ed0c983170f49c628a98e6ec240099c07a3b4f
MicCabeza22 <cabeza.michael@yahoo.com>
2017-12-12 09:46:29 -0600

    Fixed formatting a bit.

--- where_exercises.sql
+++ where_exercises.sql
@@ -3,7 +3,8 @@ USE employees;
 SELECT * FROM employees WHERE (
   first_name = 'Irena'
   OR first_name = 'Vidya'
-  OR first_name = 'Maya')
+  OR first_name = 'Maya'
+  )
   AND gender = 'M';
 
 SELECT * FROM employees WHERE (
@@ -14,7 +15,7 @@ SELECT * FROM employees WHERE last_name LIKE 'E%e';
 
 SELECT * FROM employees WHERE (
   hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-)
+  )
   AND birth_date LIKE '%%%%-12-25';
 
 SELECT * FROM employees WHERE

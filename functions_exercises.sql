d077d8add7af6279b4ec305c38d72dc434236f48
MicCabeza22 <cabeza.michael@yahoo.com>
2017-12-13 10:14:16 -0600

    Renamed a generated column.

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -21,7 +21,7 @@ SELECT CONCAT(
   'Employed for ',
   datediff(CURDATE(), hire_date),
   ' days.'
-  )
+  ) AS 'days_employed'
   FROM employees WHERE (
   hire_date BETWEEN '1990-01-01' AND '1999-12-31'
   )
